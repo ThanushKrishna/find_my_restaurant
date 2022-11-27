037eecce246843a5cb91e1e02dd29421c09a0a5d
thanush,krishna <krishnathanush91@gmail.com>
2022-11-28 00:10:04 +0545

    router update

--- routes/rest.router.js
+++ routes/rest.router.js
@@ -4,4 +4,36 @@ const restaurantController = require("../controllers/restaurant.controller");
 
 rRouter.post("/", restaurantController.addRestaurant);
 
+// GET /api/restaurant/
+// This API returns details of all the restaurants present in the database.
+restRouter.get("/", restaurantController.fetchAllRestaurants)
+
+// GET /api/restaurant/categories
+// This API returns the list of all the categories of restaurants present.
+restRouter.get("/categories", restaurantController.fetchAllCategories)
+
+// GET /api/restaurant/categories/categoryName
+// This API returns details of all the restaurants of a particular category in the database.
+restRouter.get("/categories/:categoryName", restaurantController.fetchByCategory)
+
+// GET /api/restaurant/id
+// This API returns details of the restaurant with a particular id.
+restRouter.get("/:id", restaurantController.fetchById)
+
+// GET /api/restaurant/rating/ratingValue
+// This API returns details of all the restaurants with ratings greater than equal to the given rating.
+restRouter.get("/rating/:ratingValue", restaurantController.fetchByRating)
+
+// PUT /api/restaurant/id
+// This API updates existing details of the restaurant with a particular id.
+restRouter.put("/:id", restaurantController.updateById)
+
+// DELETE /api/restaurant/id 
+// This API deletes existing details of the restaurant with a particular id. After successful deletion, the following response should be returned with status code 200.
+restRouter.delete("/:id", restaurantController.deleteById)
+
+// DELETE  /api/restaurant/
+// This API deletes all existing details of the restaurants
+restRouter.delete("", restaurantController.deleteAll)
+
 module.exports = rRouter
